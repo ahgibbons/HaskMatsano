55e4f2ffe2903295ac0d7c10c22d1dff6a168f37
Andrew.Windows <andrew.h.gibbons@gmail.com>
2016-06-13 10:32:12 +0900

    About to change list functions to BS functions

--- src/Set1.hs
+++ src/Set1.hs
@@ -7,9 +7,15 @@ import qualified Data.ByteString.Char8 as BSC
 import Data.Bits
 import Data.Char
 import Data.List
-import Data.Ord
-import System.IO
+import Data.Ord (comparing)
+import System.IO (readFile)
 import Data.Word8
+import Data.Maybe (fromJust)
+import qualified Data.ByteString.Base64 as B64
+
+import Crypto.Cipher.AES
+import Crypto.Cipher.Types
+import Crypto.Error
 
 -- Challenge 1 inText
 -- Hex to B64
@@ -28,7 +34,7 @@ ch2_t2 :: HexString
 ch2_t2 = "686974207468652062756c6c277320657965"
 
 chall2 :: HexString
-chall2 = showB16 $ (readB16 ch2_t1) `xor` (readB16 ch2_t2)
+chall2 = showB16Int $ (readB16Int ch2_t1) `xor` (readB16Int ch2_t2)
 
 chall2_res :: HexString
 chall2_res = "746865206b696420646f6e277420706c6179"
@@ -40,19 +46,63 @@ ch3_instring :: HexString
 ch3_instring = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
 
 
-ch3_alldecodes = map (xorCharBS (hexToString ch3_instring)) [_A.._z]
-chall3 = head . reverse . sortBy (comparing fst) $ scores
-  where
-    scores = zip (map englishTest ch3_alldecodes) ch3_alldecodes
+chall3 = decodeBSXOR [_0.._z] $ readB16 ch3_instring
 
 -- Challenge 4
 filePath = "4.txt"
 
 chall4 = do
-  fileText <- readFile filePath
-  let fileLines = map BSC.pack $ lines fileText
-      allDecodes hs = map (xorCharBS $ hexToString hs) [_A.._z]
-      topLines = map (\hs -> head . reverse . sortBy (comparing fst)
-                     . zip (map englishTest (allDecodes hs)) $ (allDecodes hs)) fileLines
-  print $ head . reverse . sortBy (comparing fst) $ topLines
-  print topLines
+  fileText <- BS.readFile filePath
+  let inlines = BSC.lines fileText
+      scores = map (decodeBSXOR [_0.._z] . readB16) inlines
+  print $ head . reverse . sortBy (comparing fst') $ scores
+    where
+      fst' (a,_,_) = a
+
+-- Challenge 5
+ch5_inText = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal" :: BS.ByteString
+ch5_key = "ICE" :: BS.ByteString
+
+chall5 = showB16 $ repeatXOR ch5_key ch5_inText
+
+
+-- Challenge 6
+ch6_keyRange = [2..40]
+ch6_t1 = "this is a test" :: BS.ByteString
+ch6_t2 = "wokka wokka!!!" :: BS.ByteString
+
+inFile = BS.readFile "6.txt"
+inTextIO = do
+  inText <- inFile
+  return $ fromJust . readB64 . BS.filter (/= _lf) $ inText
+
+chall6 = do
+  inText <- inTextIO
+  let k = BS.pack $ map (\(_,_,a) -> a) $ decodeRepeatXOR 29 inText
+      t = repeatXOR k inText
+  return t
+
+
+-- Challenge 7
+ch7_key = "YELLOW SUBMARINE" :: BS.ByteString
+
+ch7_inText = BS.readFile "7.txt"
+
+chall7 = do
+  fileText <- ch7_inText
+  let ctext = fromJust . readB64 . BS.filter (/= _lf) $ fileText
+      CryptoPassed aes128 = cipherInit ch7_key :: CryptoFailable AES128
+      ptext = ecbDecrypt aes128 ctext
+  print ptext
+
+-- Challenge 8
+ch8_inText = BS.readFile "8.txt"
+
+
+{-}
+chall8 = do
+  fileText <- ch8_inText
+  let tlines = BSC.lines fileText
+      chunkLines = map (chunksOf 32 . BS.unpack) tlines
+      chunkLinesBS = map BS.pack tlines
+-}
